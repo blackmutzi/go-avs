fdf3f0b00499178b80bab8284dcdcd47c880b48d
blackmutzi <this.mutzii@gmail.com>
2018-05-29 05:00:52 +0200

    add speech recognize requests

--- src/event/speechrecognize.go
+++ src/event/speechrecognize.go
@@ -3,12 +3,46 @@ package event
 import (
 	"io/ioutil"
 	"strings"
+	"http2"
 )
 
 type SpeechRecognize struct {
 	Event *SynchronizeStateEvent
 	MessageID string
 	DialogRequestID string
+	InitiatorType string
+	StartIndexSamples string
+	EndIndexSamples string
+}
+/*
+	Speech Recognize WakeWord Profil
+
+	Reference:
+	https://developer.amazon.com/de/docs/alexa-voice-service/streaming-requirements-for-cloud-based-wake-word-verification.html
+ */
+func NewSpeechRecognizeWakeWordProfil( sync * SynchronizeStateEvent ) * SpeechRecognize {
+	speech := &SpeechRecognize{}
+	speech.Event = sync
+	speech.MessageID = NewMessageId()
+	speech.DialogRequestID = NewDialogReqId()
+	speech.InitiatorType = "WAKEWORD"
+	speech.StartIndexSamples = "8000"
+	speech.EndIndexSamples = "16000"
+	return speech
+}
+
+/*
+	Default Speech Recognize TAP Profil
+ */
+func NewSpeechRecognizeTAPProfil( sync * SynchronizeStateEvent ) * SpeechRecognize {
+	speech := &SpeechRecognize{}
+	speech.Event = sync
+	speech.MessageID = NewMessageId()
+	speech.DialogRequestID = NewDialogReqId()
+	speech.InitiatorType = "TAP"
+	speech.StartIndexSamples = "0"
+	speech.EndIndexSamples = "0"
+	return speech
 }
 
 func ( s * SpeechRecognize ) CreateSpeechRecognizeEvent() string {
@@ -28,8 +62,33 @@ func ( s * SpeechRecognize ) CreateSpeechRecognizeEvent() string {
 	content = strings.Replace( content , "{{PROFILE_STRING}}" , "NEAR_FIELD" , -1 )
 	content = strings.Replace( content , "{{FORMAT_STRING}}" , "AUDIO_L16_RATE_16000_CHANNELS_1" , -1 )
 
-	content = strings.Replace( content , "{{TYPE_STRING}}" , "TAP" , -1 )
-	content = strings.Replace( content , "{{LONG}}" , "0" , -1 )
+	content = strings.Replace( content , "{{TYPE_STRING}}" , s.InitiatorType , -1 )
+	content = strings.Replace( content , "{{START_INDEX_SAMPLES}}" , "0" , -1 )
+	content = strings.Replace( content , "{{END_INDEX_SAMPLES}}" , "0" , -1 )
+	content = strings.Replace( content , "{{PAYLOAD_TOKEN}}" , "" , -1 )
 
 	return content
 }
+
+/*
+	Create a new SpeechRecognize Request with the WakeWord Profil
+ */
+func NewSpeechRecognizeWakeWordRequest( pcmBytes []int16 ) ( *http2.Request ) {
+	recognize := NewSpeechRecognizeWakeWordProfil( NewSyncStateEvent() )
+	recogInfo := NewTransportInfo("1390402302040")
+	req := &http2.Request{}
+	req.TransportInfo = recogInfo.CreateMessageWithAudioContent( recognize.CreateSpeechRecognizeEvent() , recogInfo.CreateAudio( pcmBytes ) )
+	return req
+}
+
+/*
+	Create a new SpeechRecognize Request with the WakeWord Profil
+ */
+func NewSpeechRecognizeTAPRequest( pcmBytes []int16 ) ( *http2.Request ) {
+	recognize := NewSpeechRecognizeTAPProfil( NewSyncStateEvent() )
+	recogInfo := NewTransportInfo("1390402302040")
+	req := &http2.Request{}
+	req.TransportInfo = recogInfo.CreateMessageWithAudioContent( recognize.CreateSpeechRecognizeEvent() , recogInfo.CreateAudio( pcmBytes ) )
+	return req
+}
+
